9e8aaa1b26b7d738f2006a15d636e588630b3456
Klin-Coderse <obcbeats@gmail.com>
2023-03-20 00:58:27 +0300

    cleanup

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,7 @@
-
 import HomeScreen from "../components/Homepage/HomeScreen";
 
-import connectToDb from "../backend/connectToDb";
-
-
 const Home = () => {
-  return (
-   <HomeScreen />
-  );
+  return <HomeScreen />;
 };
 
 export default Home;
-
-export async function getServerSideProps({ req, res }) {
-  const db = await connectToDb();
-
-  return {
-    props: {},
-  };
-}
