9cb1b03966b72ea7884b4784b40161c00b6e216f
jc-coder <josephchimezie2003@gmail.com>
2023-03-19 22:57:47 +0100

    add hackaton and group models

---
+++ backend/models/groupModel.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const groupSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    members: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+const Group = mongoose.model("Group", groupSchema);
+module.exports = Group;
---
+++ backend/models/hackatonModel.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const hackatonSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    version: {
+      type: Number,
+      required: true,
+    },
+    groups: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Group",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+const Hackaton = mongoose.model("Hackaton", hackatonSchema);
+module.exports = Hackaton;
