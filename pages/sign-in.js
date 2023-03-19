c0b9762c9274244b58df32604fe3158f7ace0ad6
Klin-Coderse <obcbeats@gmail.com>
2023-03-20 02:34:42 +0300

    refactor

--- pages/sign-in.js
+++ pages/sign-in.js
@@ -1,17 +1,7 @@
-import React from 'react';
+import React from "react";
 
 const SignIn = () => {
-    return (
-        <div className={'w-100 h-[100vh] flex flex-col items-center justify-center'}>
-            <div className="text-center mb-20">
-                <h2 className='text-xxl'>Login</h2>
-                <p className="text-gray-600">Welcome back, let's get you started</p>
-            </div>
-            <div className="max-w-[500px] mx-auto min-h-[200px] rounded w-100">
-
-            </div>
-        </div>
-    );
+  return <div className={"w-100"}></div>;
 };
 
 export default SignIn;
