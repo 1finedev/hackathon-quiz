d937f4d3228b4d145c7b64cf7b1571ecbe6a8aa8
289volts <289volts@gmail.com>
2023-03-19 22:09:04 +0100

    [FEAT] Added font to app

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
 import "../styles/globals.css";
+import { Manrope } from "next/font/google";
 
+const manrope = Manrope({
+	weights: [400, 500, 600, 700],
+	styles: ["normal", "italic"],
+	subsets: ["latin"],
+});
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+	return (
+		<main className={manrope.className}>
+			<Component {...pageProps} />
+		</main>
+	);
 }
 
 export default MyApp;
