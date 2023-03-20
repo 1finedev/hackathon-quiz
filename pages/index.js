f22aa955e2701e8c2f1ed4c3ca40abeac81f6da1
Klin-Coderse <obcbeats@gmail.com>
2023-03-20 16:40:46 +0300

    FIX: import error on index and added temporary redirect

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,21 @@
 import Link from "next/link";
-import SignUp from "../components/signup/SignUp";
 
 import { useSession } from "next-auth/react";
 
 const Home = () => {
-  return (
-    <main className="min-h-full flex justify-center items-center">
-      <SignUp />
-    </main>
-  );
+  return <main className="min-h-full flex justify-center items-center"></main>;
 };
 
 export default Home;
 
 //  to protect this page from unauthorized access
 // Home.auth = true;
+
+export const getServerSideProps = async () => {
+  return {
+    redirect: {
+      destination: "/sign-up",
+      permanent: false,
+    },
+  };
+};
