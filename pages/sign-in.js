12a042338358e3d868312198e879c5fe871d1915
Emmanuel Ufere <clintonufere@gmail.com>
2023-03-20 04:17:11 +0100

    Fix: fixed undefined as a param to mobile

--- pages/sign-in.js
+++ pages/sign-in.js
@@ -24,7 +24,7 @@ const SignIn = () => {
 
         const response = await signIn("credentials", {
             redirect: false,
-            mobile: fields.mobile,
+            mobile: fields.number,
             password: fields.password,
         });
         if (!response.ok) {
