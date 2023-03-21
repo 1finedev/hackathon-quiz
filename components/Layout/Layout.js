b6ed30c89198dfe8cd7d65dfde19da6b553b4280
Olulanke-Mainasara <95680556+Olulanke-Mainasara@users.noreply.github.com>
2023-03-21 12:09:59 +0100

    Fixing the merge conflict

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -4,15 +4,24 @@ import Footer from "./Footer";
 import Header from "./Header";
 
 const Layout = ({ children, className }) => {
-	const { pathname } = useRouter();
-	return (
-		<>
-			{pathname === "/sign-up" || pathname === "/sign-in" ? null : <Header className={className} />}
-			<main className={`${className} text-white pt-[3rem] h-[100vh] overflow-scroll flex justify-center items-center`}>{children} </main>
-			{pathname === "/sign-up" || pathname === "/sign-in" || pathname === "/quiz" ? null : <Footer className={className} />}
-			
-		</>
-	);
+  const { pathname } = useRouter();
+  return (
+    <>
+      {pathname === "/sign-up" || pathname === "/sign-in" ? null : (
+        <Header className={className} />
+      )}
+      <main
+        className={`${className} text-white pt-[3rem] h-[100vh] overflow-scroll flex justify-center items-center`}
+      >
+        {children}{" "}
+      </main>
+      {pathname === "/sign-up" ||
+      pathname === "/sign-in" ||
+      pathname === "/quiz" ? null : (
+        <Footer className={className} />
+      )}
+    </>
+  );
 };
 
 export default Layout;
