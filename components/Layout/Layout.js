cca7c797f39acb0aa2d903dc71100d62380aad02
Olulanke-Mainasara <95680556+Olulanke-Mainasara@users.noreply.github.com>
2023-03-21 03:34:28 +0100

    Built the quiz page

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -8,8 +8,8 @@ const Layout = ({ children, className }) => {
 	return (
 		<>
 			{pathname === "/sign-up" || pathname === "/sign-in" ? null : <Header className={className} />}
-			<main className={`${className} text-white pt-[3rem] h-[100vh] flex justify-center items-center`}>{children} </main>
-			{pathname === "/sign-up" || pathname === "/sign-in" ? null : <Footer className={className} />}
+			<main className={`${className} text-white pt-[3rem] h-[100vh] overflow-scroll flex justify-center items-center`}>{children} </main>
+			{pathname === "/sign-up" || pathname === "/sign-in" || pathname === "/quiz" ? null : <Footer className={className} />}
 			
 		</>
 	);
