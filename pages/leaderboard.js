a71e9e0fdf93573646accb234eff696005dcbcd5
unknown <judgepaulogebe3@gmail.com>
2023-03-22 14:18:12 +0100

    add leaderboard page

--- pages/leaderboard.js
+++ pages/leaderboard.js
@@ -1,60 +1,29 @@
-import { useState, useEffect } from 'react';
-import data from '../components/Layout/database';
+import React from "react";
 
-function Leaderboard() {
-  const [people, setPeople] = useState(data);
-  const [filter, setFilter] = useState('all');
-
-  useEffect(() => {
-    // fetch data from API or JSON file
-    setPeople(data);
-  }, []);
-
-  const filterPeople = (value) => {
-    setFilter(value);
-
-    if (value === 'thisWeek') {
-      const filteredData = data.filter(person => person.score >= 50);
-      setPeople(filteredData);
-    } else if (value === 'thisMonth') {
-      const filteredData = data.filter(person => person.score >= 70);
-      setPeople(filteredData);
-    } else {
-      setPeople(data);
-    }
-  };
-
+export default function LeaderBoard() {
   return (
-    <div className="container mx-auto px-4 py-8">
-      {/* buttons */}
-      <div className="flex justify-center space-x-4 my-4">
-        <button className={`py-2 px-4 rounded-md ${filter === 'thisWeek' ? 'bg-blue-500 text-white' : 'bg-gray-200 text-gray-500 hover:bg-blue-500 hover:text-white'}`} onClick={() => filterPeople('thisWeek')}>
-          This Week
-        </button>
-        <button className={`py-2 px-4 rounded-md ${filter === 'thisMonth' ? 'bg-blue-500 text-white' : 'bg-gray-200 text-gray-500 hover:bg-blue-500 hover:text-white'}`} onClick={() => filterPeople('thisMonth')}>
-          This Month
-        </button>
-        <button className={`py-2 px-4 rounded-md ${filter === 'all' ? 'bg-blue-500 text-white' : 'bg-gray-200 text-gray-500 hover:bg-blue-500 hover:text-white'}`} onClick={() => filterPeople('all')}>
-          All Time
-        </button>
+    <div>
+      <div className="text-center">
+        <h4 className="font-semibold text-4xl lg:text-5xl mb-2">Leaderboard🏆</h4>
+        <p className="text-[#C9C9C9] mb-2">Rankings of all Registered Users</p>
       </div>
-
-      {/* wall of fame */}
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
-        {people.map(person => (
-          <div key={person.id} className="bg-white rounded-md p-4">
-            {/* person info */}
-            <h2 className="text-lg font-bold text-blue-500 mb-2">{person.name}</h2>
-            <p className="text-gray-500 mb-2">{person.position}</p>
-            <p className="text-blue-500 font-bold text-xl">{person.score}</p>
+      <div className="mx-4 w-[95vw] md:w-[30rem]">
+        <div className="bg-[#06BA6B] grid grid-cols-3">
+          <button className="text-center text-lg font-semibold py-4 hover:bg-[#018a51]">Overall</button>
+          <button className="text-center text-lg font-semibold py-4 hover:bg-[#018a51]">Javascript</button>
+          <button className="text-center text-lg font-semibold py-4 hover:bg-[#018a51]">React</button>
+        </div>
+        <div>
+          <div className="flex justify-between py-5 bg-[#202020] px-5">
+            <div className="flex">
+              <span className="bg-[#FFCA28] py-0.5 px-1.5 rounded-full font-semibold">1.</span>
+              <h4 className="my-auto ml-3 font-semibold">Glory</h4>
+            </div>
+            <h4 className="my-auto font-semibold">20pts</h4>
           </div>
-        ))}
+          <hr className="bg-[#808080]" />
+        </div>
       </div>
     </div>
-  );
+  )
 }
-
-export default Leaderboard;
-
-
-
