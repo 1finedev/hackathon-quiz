3312beef191036268e35e45c20f77925aadff46b
Klin-Coderse <obcbeats@gmail.com>
2023-03-22 20:54:52 +0300

    Fixed Deployment Issues

--- pages/leaderboard.js
+++ pages/leaderboard.js
@@ -1,13 +1,115 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
+import connectToDb from "../backend/connectToDb";
+import Quiz from "../backend/models/quizModel";
 
 // get static props
 export async function getStaticProps() {
-  const response = await axios.get(
-    `${process.env.NEXT_PUBLIC_API_URL}/quiz/leaderboard`
-  );
+  await connectToDb();
+
+  const categoriesResult = await Quiz.aggregate([
+    {
+      $match: {
+        //   totalAttempted: { $gte: 20 },
+      },
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "user",
+        foreignField: "_id",
+        as: "user",
+      },
+    },
+    {
+      $group: {
+        _id: "$category",
+        users: {
+          $push: {
+            user: "$user",
+            totalCorrect: "$totalCorrect",
+            totalAttempted: "$totalAttempted",
+            percentage: {
+              $multiply: [
+                { $divide: ["$totalCorrect", "$totalAttempted"] },
+                100,
+              ],
+            },
+          },
+        },
+      },
+    },
+    {
+      $unwind: "$users",
+    },
+    {
+      $sort: {
+        "users.percentage": -1,
+      },
+    },
+    {
+      $group: {
+        _id: "$_id",
+        users: {
+          $push: "$users",
+        },
+      },
+    },
+    {
+      $project: {
+        _id: 0,
+        category: "$_id",
+        users: 1,
+      },
+    },
+  ]);
+
+  const Overall = await Quiz.aggregate([
+    {
+      $lookup: {
+        from: "users",
+        localField: "user",
+        foreignField: "_id",
+        as: "user",
+      },
+    },
+    {
+      $group: {
+        _id: "$user",
+        totalCorrect: { $sum: "$totalCorrect" },
+        totalAttempted: { $sum: "$totalAttempted" },
+        user: { $first: "$user" },
+      },
+    },
+    {
+      $project: {
+        _id: 0,
+        totalCorrect: 1,
+        totalAttempted: 1,
+        user: 1,
+        percentage: {
+          $multiply: [{ $divide: ["$totalCorrect", "$totalAttempted"] }, 100],
+        },
+      },
+    },
+    {
+      $sort: {
+        percentage: -1,
+      },
+    },
+    {
+      $match: {
+        //   totalAttempted: { $gte: 40 },
+      },
+    },
+  ]);
+
+  const data = {
+    Overall,
+    categoriesResult,
+  };
+
   return {
-    props: { data: response.data.results }, // nextjs serialization issue
+    props: { data: JSON.parse(JSON.stringify(data)) }, // nextjs serialization issue
     revalidate: 60, // Regenerate list every minute
   };
 }
