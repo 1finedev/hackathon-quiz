2caf811d1d73956c73673b3e045d6515dbf25e2d
gideon-del <gideonchidi471@gmial.com>
2023-03-22 10:01:33 +0100

    Added a little animation

--- pages/score.js
+++ pages/score.js
@@ -1,12 +1,29 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 
 const Score = () => {
+  const borderRef = useRef();
+ useEffect(() => {
+  let progressEnd = 90;
+  let progressStart =0;
+   const progress = setInterval(() => {
+       progressStart++
+       if(progressStart === progressEnd){
+        clearInterval(progress)
+       }
+       borderRef.current.style.background = `conic-gradient(rgba(6, 186, 107, 1)${progressStart * 3.6}deg, rgba(6, 186, 107, 0.25) 0deg)`
+      },10)
+
+      return () => {
+        clearInterval(progress)
+      }
+ })
+
   return (
     <section className='flex flex-col items-center gap-3'>
         <div className='stack w-min mx-auto'>
-          <div className=' rounded-full scale-110 animate-load transition-all duration-500 ease-in' style={{
-              background:'conic-gradient(rgba(6, 186, 107, 1) 81%, rgba(6, 186, 107, 0.25) 81%)' 
-          }}>
+          <div className=' rounded-full scale-110 animate-load transition-all duration-500 ease-in' ref={borderRef} style={{
+            background:`conic-gradient(rgba(6, 186, 107, 1) 0deg, rgba(6, 186, 107, 0.25) 0deg)`
+          }} >
          
           </div>
             <div className='p-6 aspect-square grid relative after:absolute after:inset-0 after:w-full   after:rounded-full    place-items-center w-fit rounded-full bg-black' >
